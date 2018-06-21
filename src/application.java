9979c72cc260aa6ced9addb1cbdd545b46d98ded
Dev Patel <dev.patel@ticketmaster.com>
2018-06-21 13:14:53 -0700

    new ignore

--- src/application.java
+++ src/application.java
@@ -1,15 +1,21 @@
 
 public class application {
+    private int total =0;
 
     public static void main(String[] args) {
         System.out.println("testing...");
         System.out.println("new");
-
     }
 
+    public void forLoop(int x, int y) {
+        for (int i = 0; i <= x; i++)
+            for (int j = 0; j <= y; j++) {
+                total += x = y;
+                System.out.println(total);
+            }
 
 
-
-    // work
-
+        // work
+        //comments
+    }
 }
