9a6e749bc76473bae7199d340c7f4a0b8abd2b79
Dev Patel <dev.patel@ticketmaster.com>
2018-06-21 14:55:46 -0700

    Iceland enters

--- src/WorldCup.java
+++ src/WorldCup.java
@@ -4,7 +4,11 @@ public class WorldCup {
         int random = (int) Math.random();
         if(random==0)
             System.out.println("Spain Wins!");
-        if(random==0)
+        if(random==1)
             System.out.println("Mexico Wins!");
+        if(random*2 > .05){
+            System.out.println("ICELAND FOR THE WIN");
+        }
+
     }
 }
