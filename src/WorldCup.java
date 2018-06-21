7e6ba88d13b4b2a3d73fde780354ad814e8fbfe2
Dev Patel <dev.patel@ticketmaster.com>
2018-06-21 13:56:18 -0700

    World Cup

--- src/WorldCup.java
+++ src/WorldCup.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class WorldCup {
+
+    public void theBest(){
+        int random = (int) Math.random();
+        if(random==0)
+            System.out.println("Spain Wins!");
+        if(random==0)
+            System.out.println("Mexico Wins!");
+    }
 }
