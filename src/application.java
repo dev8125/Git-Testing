6f1ccbbd040decdfff4aefff58d3a63545b206c6
Dev Patel <dev.patel@ticketmaster.com>
2018-06-21 11:31:32 -0700

    added number class

--- src/application.java
+++ src/application.java
@@ -1,24 +1,21 @@
 
 public class application {
-    private static int total =0;
+    private int total =0;
 
     public static void main(String[] args) {
         System.out.println("testing...");
         System.out.println("new");
-        System.out.println(total);
     }
 
-    public void forLoop(int x, int y){
-        for(int i=0; i <=x; i++)
-            for(int j=0; j<=y;j++)
-            {
-                total+= x=y;
-            } }
+    public void forLoop(int x, int y) {
+        for (int i = 0; i <= x; i++)
+            for (int j = 0; j <= y; j++) {
+                total += x = y;
+                System.out.println(total);
+            }
 
 
-
-
-    // work
-    //comments
-
+        // work
+        //comments
+    }
 }
---
+++ src/numberClass.java
@@ -0,0 +1,8 @@
+public class numberClass {
+
+    public static void main(String[] args) {
+        for(int i=0; i<= (int) Math.random(); i++){
+            System.out.println(i);
+        }
+    }
+}
