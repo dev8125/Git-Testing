4086f3cbf0ec92e4263a69de6951ea76fcae92dd
Dev Patel <dev.patel@ticketmaster.com>
2018-06-21 11:10:06 -0700

    created a for loop

--- src/application.java
+++ src/application.java
@@ -1,15 +1,24 @@
 
 public class application {
+    private static int total =0;
 
     public static void main(String[] args) {
         System.out.println("testing...");
         System.out.println("new");
-
+        System.out.println(total);
     }
+
+    public void forLoop(int x, int y){
+        for(int i=0; i <=x; i++)
+            for(int j=0; j<=y;j++)
+            {
+                total+= x=y;
+            } }
 
 
 
 
     // work
+    //comments
 
 }
